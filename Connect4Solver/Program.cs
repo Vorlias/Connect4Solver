4948d72dbf0d529c1797bc474afd30be73c73fae
Jonathan Holmes <Vorlias@users.noreply.github.com>
2026-09-10 16:25:53 +1200

    Add horiz/vert checks - helps w/ diag check

--- Connect4Solver/Program.cs
+++ Connect4Solver/Program.cs
@@ -20,48 +20,58 @@ int GetConnect4Winner(int[][] board)
     {
         if (board[row].Length != columnCount) throw new ArgumentException($"Invalid row count, should be {columnCount} for inner array of {nameof(board)}.");
         
-        // If we have less than 4 rows left, then diagonally no winners can happen
-        var canCheckDiagonally = row <= rowCount - requiredTokenCount;
+        // Less than 4 rows left = no vertical possible
+        var canCheckVertical = row <= rowCount - requiredTokenCount;
 
         for (var col = 0; col < board[row].Length; col++)
         {
+            // Less than 4 columns = no horizontal possible
+            var canCheckHorizontal = col <= columnCount - requiredTokenCount;
+            // If we have less than 4 rows and cols left, then diagonally no winners can happen
+            var canCheckDiagonal = canCheckVertical && canCheckHorizontal;
+            
             // the current token we're checking
             var startToken = board[row][col];
             if (startToken == 0) continue; // ignore empty spaces
 
             tokenCount = 1;
-            
-            // horizontal lookahead from current position
-            for (var nextCol = col + 1; nextCol < board[row].Length; nextCol++)
+
+            if (canCheckHorizontal)
             {
-                if (tokenCount >= requiredTokenCount) break;
+                // horizontal lookahead from current position
+                for (var nextCol = col + 1; nextCol < board[row].Length; nextCol++)
+                {
+                    if (tokenCount >= requiredTokenCount) break;
                 
-                var nextToken = board[row][nextCol];
-                if (nextToken == startToken)
-                {
-                    tokenCount += 1;
-                }
-                else
-                {
-                    tokenCount = 1;
-                    break; // at this point don't need to continue looking
+                    var nextToken = board[row][nextCol];
+                    if (nextToken == startToken)
+                    {
+                        tokenCount += 1;
+                    }
+                    else
+                    {
+                        tokenCount = 1;
+                        break; // at this point don't need to continue looking
+                    }
                 }
             }
 
-            // vertical lookahead
-            for (var nextRow = row + 1; nextRow < board.Length; nextRow++)
-            {
-                if (tokenCount >= requiredTokenCount) break;
-                
-                var nextToken = board[nextRow][col];
-                if (nextToken == startToken)
+            if (canCheckVertical) {
+                // vertical lookahead
+                for (var nextRow = row + 1; nextRow < board.Length; nextRow++)
                 {
-                    tokenCount += 1;
-                }
-                else
-                {
-                    tokenCount = 1;
-                    break; // at this point don't need to continue looking
+                    if (tokenCount >= requiredTokenCount) break;
+                    
+                    var nextToken = board[nextRow][col];
+                    if (nextToken == startToken)
+                    {
+                        tokenCount += 1;
+                    }
+                    else
+                    {
+                        tokenCount = 1;
+                        break; // at this point don't need to continue looking
+                    }
                 }
             }
             
@@ -70,7 +80,7 @@ int GetConnect4Winner(int[][] board)
             // \
             // /
             // we're gonna start off greedy here, can optimise later to be more lazy based on knowledge like how far we are from start/end vs what directions are realistic
-            if (canCheckDiagonally)
+            if (canCheckDiagonal)
             {
                 
             }
